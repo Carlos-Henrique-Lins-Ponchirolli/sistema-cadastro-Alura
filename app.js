16d3205ee6b2778ae0890b1e11d37fbebd485163
Carlos-Henrique-Lins-Ponchirolli <ch.ponchirolli@gmail.com>
2023-03-12 15:41:58 -0300

    Voltei um passo na aplicação

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
 console.log("Rodando o sistema de gerenciamento de jogos");
 console.log("Aplicação está se conectando ao banco de dados");
 console.log("Última modificação 1");
-console.log("Enviando para o GitHub!!!");
